c7b8662f86b2f3e601530c967db3c791d0113c5b
bulut67 <mahirbulut67@gmail.com>
2020-08-09 23:37:53 +0300

    sorun cozuldu

--- src/git_project_ikinci/deneme.java
+++ src/git_project_ikinci/deneme.java
@@ -8,12 +8,7 @@ public class deneme {
 			System.out.println("ikinci satiri degistirdim");
 			System.out.println("gitHub'dan yazdigim satir");
 			System.out.println("Mahir bulut");
-<<<<<<< HEAD
 			System.out.println("Bulut cakisma");
-=======
-                        System.out.println("cakismayalim pls");
-		     
->>>>>>> refs/remotes/origin/master
 
 	}
 
